5dd123cfa304e97bdb359480483badd56a17052d
Xavier Garrido <xavier.garrido@gmail.com>
2017-10-21 10:50:06 +0200

    better parsing of bayeux options

--- packages/snemo/bayeux.zsh
+++ packages/snemo/bayeux.zsh
@@ -26,37 +26,47 @@ function bayeux::configure()
 {
     __pkgtools__at_function_enter bayeux::configure
 
-    pkgman setup brew
+    local brew_install_dir=$(__pkgman::get_install_dir brew master)
+    if [[ -z ${brew_install_dir} ]]; then
+        pkgtools__msg_error "Missing brew install!"
+        __pkgtools__at_function_exit
+        return 1
+    fi
+
+    # Parse Bayeux options
     local bayeux_options="
         -DCMAKE_BUILD_TYPE:STRING=Release
         -DCMAKE_INSTALL_PREFIX=${location}/install
-        -DCMAKE_PREFIX_PATH=$(dirname $(pkgtools__get_binary_path brew))/..
+        -DCMAKE_PREFIX_PATH=${brew_install_dir}/brew
         -DBAYEUX_CXX_STANDARD=14 "
-    while [ -n "$1" ]; do
-        local token="$1"
-        if [[ ${token[0,1]} = - ]]; then
-            local opt=${token}
-            if [[ ${opt} = --with-test ]]; then
-                bayeux_options+="-DBAYEUX_ENABLE_TESTING=ON "
-            elif [[ ${opt} = --without-test ]]; then
-                bayeux_options+="-DBAYEUX_ENABLE_TESTING=OFF "
-            elif [[ ${opt} = --with-doc ]]; then
-                bayeux_options+="-DBAYEUX_WITH_DOCS=ON "
-            elif [[ ${opt} = --without-doc ]]; then
-                bayeux_options+="-DBAYEUX_WITH_DOCS=OFF "
-            elif [[ ${opt} = --with-warning ]]; then
-                bayeux_options+="-DBAYEUX_COMPILER_ERROR_ON_WARNING=ON "
-            elif [[ ${opt} = --without-warning ]]; then
-                bayeux_options+="-DBAYEUX_COMPILER_ERROR_ON_WARNING=OFF "
-            fi
+    local args=($@)
+    local -A opts=(with-test       BAYEUX_ENABLE_TESTING
+                   with-doc        BAYEUX_WITH_DOCS
+                   without-warning BAYEUX_COMPILER_ERROR_ON_WARNING)
+    for k in "${(@k)opts}"; do
+        if [[ ${args[(r)--$k]} == --$k ]]; then
+            bayeux_options+="-D${opts[$k]}="
+            [[ $k == without-* ]] && bayeux_options+="OFF " || bayeux_options+="ON "
+        else
+            # elif [[ ${args[(r)--without-$k]} == --without-$k ]]; then
+            bayeux_options+="-D${opts[$k]}="
+            [[ $k == without-* ]] && bayeux_options+="ON " || bayeux_options+="OFF "
         fi
-        shift 1
     done
-    pkgtools__msg_devel "bayeux_options=${bayeux_options}"
-
     if $(pkgtools__has_binary ninja); then
         bayeux_options+="-G Ninja -DCMAKE_MAKE_PROGRAM=$(pkgtools__get_binary_path ninja)"
     fi
+    pkgtools__msg_devel "bayeux_options=${bayeux_options}"
+
+    # Compiler options
+    local cxx="g++ -Wno-noexcept-type"
+    local cc="gcc"
+    if $(pkgtools__has_binary ccache); then
+        cxx="ccache ${cxx}"
+        cc="ccache ${cc}"
+    fi
+    pkgtools__reset_variable CXX ${cxx}
+    pkgtools__reset_variable CC ${cc}
 
     local ret=0
     pkgtools__enter_directory ${location}/build
@@ -66,21 +76,40 @@ function bayeux::configure()
         ret=1
     fi
     pkgtools__exit_directory
-    pkgman unsetup brew
     __pkgtools__at_function_exit
     return ${ret}
+}
+
+function bayeux::build()
+{
+    __pkgtools__at_function_enter bayeux::build
+    if $(pkgtools__has_binary ninja); then
+        ninja -C ${location}/build install
+    elif $(pkgtools__has_binary make); then
+        make -j$(nproc) -C ${location}/build install
+    else
+        pkgtools__msg_error "Missing both 'ninja' and 'make' to compile bayeux !"
+        __pkgtools__at_function_exit
+        return 1
+    fi
+    if $(pkgtools__last_command_fails); then
+        pkgtools__msg_error "Compilation of bayeux fails!"
+        __pkgtools__at_function_exit
+        return 1
+    fi
+    __pkgtools__at_function_exit
+    return 0
 }
 
 function bayeux::install()
 {
     __pkgtools__at_function_enter bayeux::install
-    (
-        if [[ ! -d ${location}/${version}/.git ]]; then
-            pkgtools__msg_notice "Checkout brew from ${address}"
-            git clone ${address} ${location}/${version}
-        fi
-        bayeux::configure $@
-    )
+    if [[ ! -d ${location}/${version}/.git ]]; then
+        pkgtools__msg_notice "Checkout brew from ${address}"
+        git clone ${address} ${location}/${version}
+    fi
+    bayeux::configure $@ --with-test
+    bayeux::build $@
     __pkgtools__at_function_exit
     return 0
 }
@@ -92,6 +121,27 @@ function bayeux::uninstall()
     pkgtools__yesno_question
     if $(pkgtools__answer_is_yes); then
        rm -rf ${location}/{build,install}
+    fi
+    __pkgtools__at_function_exit
+    return 0
+}
+
+function bayeux::test()
+{
+    __pkgtools__at_function_enter bayeux::test
+    if $(pkgtools__has_binary ninja); then
+        ninja -C ${location}/build test
+    elif $(pkgtools__has_binary make); then
+        make -j$(nproc) -C ${location}/build test
+    else
+        pkgtools__msg_error "Missing both 'ninja' and 'make' to compile bayeux !"
+        __pkgtools__at_function_exit
+        return 1
+    fi
+    if $(pkgtools__last_command_fails); then
+        pkgtools__msg_error "Tests of bayeux fails!"
+        __pkgtools__at_function_exit
+        return 1
     fi
     __pkgtools__at_function_exit
     return 0
