ecfd4168c1ad34c52075d8b411a061a6407962d8
Xavier Garrido <xavier.garrido@gmail.com>
2018-06-10 09:35:23 +0200

    add enter/exit functions and fails if installation fails

--- packages/cmb/planck.zsh
+++ packages/cmb/planck.zsh
@@ -14,14 +14,18 @@ local data="${pkgman_install_dir}/../data/planck_data"
 
 function planck::dump()
 {
+    pkgtools::at_function_enter planck::dump
     pkgtools::msg_notice "planck"
     pkgtools::msg_notice " |- to      : ${location}"
     pkgtools::msg_notice " |- data    : ${data}"
     pkgtools::msg_notice " \`- from    : ${address}"
+    pkgtools::at_function_exit
+    return 0
 }
 
 function planck::install()
 {
+    pkgtools::at_function_enter planck::install
     (
         pkgman setup python2
         cd $(mktemp -d)
@@ -43,6 +47,11 @@ function planck::install()
         fi
         ./waf configure $(echo ${waf_options})
         ./waf install
+        if $(pkgtools::last_command_fails); then
+            pkgtools::msg_error "Installation of planck software fails!"
+            pkgtools::at_function_exit
+            return 1
+        fi
 
         if [ ! -d ${data} ]; then
             mkdir -p ${data}
@@ -53,29 +62,40 @@ function planck::install()
             rm -rf $(pwd)
         fi
     )
+    pkgtools::at_function_exit
+    return 0
 }
 
 function planck::uninstall()
 {
+    pkgtools::at_function_enter planck::uninstall
     pkgtools::msg_warning "Do you really want to remove planck ?"
     pkgtools::yesno_question "Answer ?"
     if $(pkgtools::answer_is_yes); then
         rm -rf ${location}
     fi
+    pkgtools::at_function_exit
+    return 0
 }
 
 function planck::setup()
 {
+    pkgtools::at_function_enter planck::setup
     pkgtools::add_path_to_LD_LIBRARY_PATH ${location}/lib
     pkgtools::set_variable CLIKDIR ${location}
     pkgtools::set_variable PLANCK_DATA ${data}
+    pkgtools::at_function_exit
+    return 0
 }
 
 function planck::unsetup()
 {
+    pkgtools::at_function_enter planck::unsetup
     pkgtools::remove_path_to_LD_LIBRARY_PATH ${location}/lib
     pkgtools::unset_variable CLIKDIR
     pkgtools::unset_variable CLIKLIBS
     pkgtools::unset_variable CLIKCFLAGS
     pkgtools::unset_variable PLANCK_DATA
+    pkgtools::at_function_exit
+    return 0
 }
--- packages/cmb/pypico.zsh
+++ packages/cmb/pypico.zsh
@@ -14,46 +14,67 @@ local data="${pkgman_install_dir}/../data/pico_data/pico3_tailmonty_v34.dat"
 
 function pypico::dump()
 {
+    pkgtools::at_function_enter pypcio::dump
     pkgtools::msg_notice "pypico"
     pkgtools::msg_notice " |- version : ${version}"
     pkgtools::msg_notice " |- data    : ${data}"
     pkgtools::msg_notice " |- to      : ${location}"
     pkgtools::msg_notice " \`- from    : ${address}"
+    pkgtools::at_function_exit
+    return 0
 }
 
 function pypico::install()
 {
+    pkgtools::at_function_enter pypcio::install
     (
         pkgman setup python2
         git clone ${address} ${location}
         cd ${location}
         python setup.py --build_cython build
         python setup.py --build_cython install --record installed_files.txt
+        if $(pkgtools::last_command_fails); then
+            pkgtools::msg_error "Installation of pypico fails!"
+            pkgtools::at_function_exit
+            return 1
+        fi
+
         if [ ! -f ${data} ]; then
             mkdir -p $(dirname ${data})
             wget -O ${data} https://owncloud.lal.in2p3.fr/index.php/s/Q0VsmRpisQQUMKL/download
         fi
     )
+    pkgtools::at_function_exit
+    return 0
 }
 
 function pypico::uninstall()
 {
+    pkgtools::at_function_enter pypcio::uninstall
     pkgtools::msg_warning "Do you really want to remove pypico ?"
     pkgtools::yesno_question "Answer ?"
     if $(pkgtools::answer_is_yes); then
         cat ${location}/installed_files.txt | xargs rm -fr
         rm -rf ${location}
     fi
+    pkgtools::at_function_exit
+    return 0
 }
 
 function pypico::setup()
 {
+    pkgtools::at_function_enter pypcio::setup
     pkgtools::set_variable PICO_CODE ${location}
     pkgtools::set_variable PICO_DATA ${data}
+    pkgtools::at_function_exit
+    return 0
 }
 
 function pypico::unsetup()
 {
+    pkgtools::at_function_enter pypcio::unsetup
     pkgtools::unset_variable PICO_CODE
     pkgtools::unset_variable PICO_DATA
+    pkgtools::at_function_exit
+    return 0
 }
