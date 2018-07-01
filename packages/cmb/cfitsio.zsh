a36bb3c89b0cc091cbd552f93b5f324973415519
Xavier Garrido <xavier.garrido@gmail.com>
2018-07-01 23:15:37 +0200

    pkgtooling

--- packages/cmb/cfitsio.zsh
+++ packages/cmb/cfitsio.zsh
@@ -13,14 +13,18 @@ local location="${pkgman_install_dir}/cfitsio/${version}"
 
 function cfitsio::dump()
 {
+    pkgtools::at_function_enter cfitsio::dump
     pkgtools::msg_notice "cfitsio"
     pkgtools::msg_notice " |- version : ${version}"
     pkgtools::msg_notice " |- from    : ${address}"
     pkgtools::msg_notice " \`- to      : ${location}"
+    pkgtools::at_function_exit
+    return 0
 }
 
 function cfitsio::install()
 {
+    pkgtools::at_function_enter cfitsio::install
     (
         cd $(mktemp -d)
         wget ${address}/cfitsio${version/./}.tar.gz
@@ -32,30 +36,41 @@ function cfitsio::install()
         cd ${location}
         ./configure --prefix=${location}/../install && make && make shared && make install
         if $(pkgtools::last_command_fails); then
-            pkgtools::msg_error "Installation of CAMEL software fails!"
+            pkgtools::msg_error "Installation of cfitsio software fails!"
             pkgtools::at_function_exit
             return 1
         fi
     )
+    pkgtools::at_function_exit
+    return 0
 }
 
 function cfitsio::uninstall()
 {
+    pkgtools::at_function_enter cfitsio::uninstall
     pkgtools::msg_warning "Do you really want to delete $(dirname ${location}) ?"
     pkgtools::yesno_question "Answer ?"
     if $(pkgtools::answer_is_yes); then
        rm -rf $(dirname ${location})
     fi
+    pkgtools::at_function_exit
+    return 0
 }
 
 function cfitsio::setup()
 {
+    pkgtools::at_function_enter cfitsio::setup
     pkgtools::set_variable CFITSIO_INCLUDE ${location}/../install/include
     pkgtools::set_variable CFITSIO_LIB ${location}/../install/lib
+    pkgtools::at_function_exit
+    return 0
 }
 
 function cfitsio::unsetup()
 {
+    pkgtools::at_function_enter cfitsio::unsetup
     pkgtools::unset_variable CFITSIO_INCLUDE
     pkgtools::unset_variable CFITSIO_LIB
+    pkgtools::at_function_exit
+    return 0
 }
