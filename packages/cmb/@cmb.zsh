f486a5aeeedc8467b10233ce96c1f7953bb514e3
Xavier Garrido <xavier.garrido@gmail.com>
2018-07-03 19:54:41 +0200

    improve readme

--- packages/cmb/@cmb.zsh
+++ packages/cmb/@cmb.zsh
@@ -68,6 +68,9 @@ function cmb::install()
     (https://github.com/xgarrido/zsh-pkgman). All the installation recipes can be viewed in
     https://github.com/xgarrido/zsh-pkgman/tree/master/packages/cmb.
 
+    To load all the software, you may source the startup scripts startup.sh or startup.csh depending
+    of your shell flavor.
+
     Below is a list of installed softwares with their associated version.
 
 EOF
